2e03e7baffce102db98fa4e5843b2fa77cba51d4
danx <danx_97@hotmail.com>
2019-04-27 03:32:43 -0500

    Get 10 largest earthquakes in the world from the last year

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -9,7 +9,37 @@ const axios = require('axios');
 // @desc    Get All locations
 // @access  Public
 router.get('/', (req, res) => {
-    res.send("Hello");
+    // Bounding box for the whole world
+    const north = 90;
+    const south = -90;
+    const east = 180;
+    const west = -180;
+
+    let count = 0;
+
+    // Get the earthquakes from the location's bounding box
+    axios.get(`http://api.geonames.org/earthquakesJSON?north=${north}&south=${south}&east=${east}&west=${west}&maxRows=500&username=danx`)
+    .then(earthquakeRes => {
+        // Set the date to a year from now
+        d = new Date();
+        d.setFullYear(d.getFullYear() - 1);
+
+        // Filter only earthquakes that happened since last year
+        let earthquakes = earthquakeRes.data.earthquakes.filter(earthquake => {
+            const earthquakeDate = new Date(earthquake.datetime);
+            if (earthquakeDate >= d && count < 10) {
+                count++;
+                return earthquake;
+            }
+        });
+
+        res.json({ earthquakes });
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    //res.send("Hello");
 });
 
 // @route   POST api/locations
